1ebcbb229024d383aed6144d9f325fa52cf6115d
Shisato <shisatoyano@gmail.com>
2021-10-11 23:36:33 +0900

    add service client node sample code

---
+++ src/service_sample/service_client.cc
@@ -0,0 +1,18 @@
+/**
+ * @file service_client.cc
+ * @author shisato yano
+ * @brief service client node sample
+ * @version 0.1
+ * @date 2021-10-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <ros/ros.h>
+#include "Ros1TrainingSamples/SrvSample.h"
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
