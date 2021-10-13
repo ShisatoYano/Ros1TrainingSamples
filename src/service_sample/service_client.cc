6f8b29794444b0ccc0f96b6be7372b8257a6a4f1
Shisato <shisatoyano@gmail.com>
2021-10-13 23:54:55 +0900

    implementing main process

--- src/service_sample/service_client.cc
+++ src/service_sample/service_client.cc
@@ -9,10 +9,31 @@
  * 
  */
 
+#include <cstdlib>
 #include <ros/ros.h>
 #include "Ros1TrainingSamples/SrvSample.h"
 
+/**
+ * @brief main process
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "service_client"); // initialize node
+
+  if (argc != 3) // input error
+  {
+    ROS_INFO("cmd : rosrun Ros1TrainingSamples service_client arg0 arg1");
+    ROS_INFO("arg0: double number, arg1: double number");
+    return 1;
+  }
+
+  ros::NodeHandle nh; // declare node handle to communicate with ros system
+
+  // declare service client
+
   return 0;
 }
