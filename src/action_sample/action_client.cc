d887047ba0d83f00f6056c5671368861ad3cd75f
Shisato <shisatoyano@gmail.com>
2021-10-15 23:50:47 +0900

    add action server

---
+++ src/action_sample/action_client.cc
@@ -0,0 +1,20 @@
+/**
+ * @file action_client.cc
+ * @author shisato yano
+ * @brief action client node sample
+ * @version 0.1
+ * @date 2021-10-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include "Ros1TrainingSamples/FibonacciAction.h"
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
